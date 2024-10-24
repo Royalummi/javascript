6ddb7dba1b67ac347c77a0d68e2799266fcd92a8
RANJITH KUMAR REDDY K <129988947+Royalummi@users.noreply.github.com>
2024-10-24 12:50:36 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "ranjith@gmail.com"
+var accountPass = "123456"
+accountCity = "Siruguppa"
+
+accountEmail = "ranjithreddy@gmail.com"
+accountPass = "147147"
+accountCity = "Bellary"
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPass, accountCity])
+
+
