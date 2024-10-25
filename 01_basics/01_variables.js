669a38dbbea99d32269b62b3866f075acc043a34
RANJITH KUMAR REDDY K <129988947+Royalummi@users.noreply.github.com>
2024-10-25 05:21:33 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,6 +3,11 @@ let accountEmail = "ranjith@gmail.com"
 var accountPass = "123456"
 accountCity = "Siruguppa"
 
+/*
+Prefer not to use var
+because of issue in bloack scope and functional scope
+*/
+
 accountEmail = "ranjithreddy@gmail.com"
 accountPass = "147147"
 accountCity = "Bellary"
